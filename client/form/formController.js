f828d6bf1adac8efeb9e784ac6b782f0e981f84a
Robert Kardos <kardos.robert.kh@gmail.com>
2015-07-12 21:12:54 +0200

    Date of birth is checked for being under 18.

--- client/form/formController.js
+++ client/form/formController.js
@@ -1,6 +1,14 @@
 formApplication.controller('formController', ['$scope', 'formService',
 	function ($scope, formService) {
 
+		var todayMinus18 = new Date();
+		todayMinus18.setFullYear(todayMinus18.getFullYear() - 18);
+
+		$scope.dateChanged = function () {
+			$scope.tooYoung = $scope.user.birthdate > todayMinus18;
+			$scope.form.birthdate.$setValidity("You are too young", !$scope.tooYoung);
+		}
+
 		$scope.submit = function (user) {
 			formService.sendUserData(user)
 				.success(function (data) {
